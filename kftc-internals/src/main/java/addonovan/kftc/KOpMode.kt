83c7e805b1ad0078d36d065271248fde735219b4
addonovan <addonovan@users.noreply.github.com>
2016-08-21 23:15:14 -0500

    Added KOpMode
    This is an OpMode specially configured just to be for the kftc library.
    It is optional, but highly recommended.

---
+++ kftc-internals/src/main/java/addonovan/kftc/KOpMode.kt
@@ -0,0 +1,105 @@
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2016 Austin Donovan (addonovan)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package addonovan.kftc
+
+import addonovan.kftc.config.Configurations
+import com.qualcomm.robotcore.eventloop.opmode.OpMode
+
+/**
+ * A Kotlin base OpMode.
+ *
+ * @author addonovan
+ * @since 8/21/16
+ */
+abstract class KOpMode : OpMode(), IConfigurable
+{
+
+    //
+    // Vals
+    //
+
+    /** The profile for the current OpMode (the subclass). */
+    private val profile = Configurations.profileFor( javaClass.kotlin );
+
+    //
+    // IConfigurable
+    //
+
+    /**
+     * Gets the boolean value from the active profile.
+     *
+     * @param[name]
+     *          The name of the boolean.
+     * @param[default]
+     *          The default value of the boolean, if it isn't found.
+     *          This can be excluded for a default value of `false`.
+     *
+     * @return The boolean value associated with the name, or the default
+     *         if no value was found.
+     */
+    final override fun get( name: String, default: Boolean ) = profile[ name, default ];
+
+    /**
+     * Gets the long value from the active profile.
+     *
+     * @param[name]
+     *          The name of the long.
+     * @param[default]
+     *          The default value of the long, if it isn't found.
+     *          This can be excluded for a default value of `false`.
+     *
+     * @return The long value associated with the name, or the default
+     *         if no value was found.
+     */
+    final override fun get( name: String, default: Long ) = profile[ name, default ];
+
+    /**
+     * Gets the double value from the active profile.
+     *
+     * @param[name]
+     *          The name of the double.
+     * @param[default]
+     *          The default value of the double, if it isn't found.
+     *          This can be excluded for a default value of `false`.
+     *
+     * @return The double value associated with the name, or the default
+     *         if no value was found.
+     */
+    final override fun get( name: String, default: Double ) = profile[ name, default ];
+
+    /**
+     * Gets the string value from the active profile.
+     *
+     * @param[name]
+     *          The name of the string.
+     * @param[default]
+     *          The default value of the string, if it isn't found.
+     *          This can be excluded for a default value of `false`.
+     *
+     * @return The string value associated with the name, or the default
+     *         if no value was found.
+     */
+    final override fun get( name: String, default: String ) = profile[ name, default ];
+
+}
