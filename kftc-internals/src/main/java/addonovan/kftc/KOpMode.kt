4fca64934168a7fea5a3e013d70f9778d42aed72
addonovan <addonovan@users.noreply.github.com>
2016-08-21 23:26:32 -0500

    Added profiles displayed in brackets
    More of a nifty feature, but I still like it

--- kftc-internals/src/main/java/addonovan/kftc/KOpMode.kt
+++ kftc-internals/src/main/java/addonovan/kftc/KOpMode.kt
@@ -24,23 +24,87 @@
 package addonovan.kftc
 
 import addonovan.kftc.config.Configurations
-import com.qualcomm.robotcore.eventloop.opmode.OpMode
+import android.support.annotation.CallSuper
+import android.text.Editable
+import android.text.TextWatcher
+import com.qualcomm.robotcore.eventloop.opmode.*
 
 /**
- * A Kotlin base OpMode.
+ * A Kotlin based OpMode.
+ *
+ * This allows for direct access to the active profile's configuration, as it
+ * already directly implements all of the [IConfigurable] methods.
+ *
+ * Another nice feature is that when a KOpMode is extended, when it is initialized
+ * and running, the active profile will be displayed in brackets after the the
+ * OpMode's name.
  *
  * @author addonovan
  * @since 8/21/16
  */
-abstract class KOpMode : OpMode(), IConfigurable
+abstract class KOpMode : OpMode(), IConfigurable, ILog
 {
 
     //
     // Vals
     //
 
+    /** The name of the OpMode as it's registered with the system. */
+    val Name: String by lazy()
+    {
+        javaClass.getAnnotation( TeleOp::class.java )?.name ?: javaClass.getAnnotation( Autonomous::class.java )!!.name;
+    }
+
     /** The profile for the current OpMode (the subclass). */
     private val profile = Configurations.profileFor( javaClass.kotlin );
+
+    /** If the system is being configured at the moment or not. */
+    private val inConfig = System.getProperty( "kftc.inconfig", "false" ).toBoolean();
+
+    //
+    // OpMode
+    //
+
+    @CallSuper
+    override fun init()
+    {
+        if ( inConfig ) return; // don't try to mess with the non-existent gui
+
+        // force the label to always say the profile name as well
+        OpModeLabel.addTextChangedListener( object : TextWatcher
+        {
+            override fun afterTextChanged( s: Editable? ){}
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int){}
+
+            override fun onTextChanged( s: CharSequence, start: Int, before: Int, count: Int )
+            {
+                val string = s.toString();
+
+                // remove our listener whenever the OpMode is over
+                if ( !string.contains( Name ) )
+                {
+                    OpModeLabel.removeTextChangedListener( this );
+                }
+
+                // if it doesn't have the configuration details, add them
+                if ( !string.contains( "[" ) )
+                {
+                    val profileName = Configurations.profileFor( this@KOpMode.javaClass.kotlin );
+
+                    // run on the ui thread so we don't get yelled at
+                    Activity.runOnUiThread {
+                        // when this is created, update the opmode label to also show
+                        // the active variant
+
+                        val text = "Op Mode: $Name [$profileName]";
+                        this@KOpMode.v( "Updating OpMode label to read: \"$text\"" );
+
+                        OpModeLabel.text = text;
+                    }
+                }
+            }
+        } );
+    }
 
     //
     // IConfigurable
