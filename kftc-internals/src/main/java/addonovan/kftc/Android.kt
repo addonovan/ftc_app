f7c5610d221900c02f75c698bac815426c3e0c61
addonovan <addonovan@users.noreply.github.com>
2016-08-27 18:44:23 -0500

    Added RobotIcon hooks
    These will indicated when the user can switch into the Configuration Activity
    if they press on the robot icon.

--- kftc-internals/src/main/java/addonovan/kftc/Android.kt
+++ kftc-internals/src/main/java/addonovan/kftc/Android.kt
@@ -23,8 +23,11 @@
  */
 package addonovan.kftc
 
+import addonovan.kftc.config.ConfigActivity
 import android.app.Activity
 import android.content.Context
+import android.content.Intent
+import android.graphics.drawable.AnimationDrawable
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
@@ -137,4 +140,32 @@ val RobotIcon: ImageView by lazy()
 val OpModeLabel: TextView by lazy()
 {
     getView( "textOpMode" ) as TextView;
+}
+
+//
+// Actions
+//
+
+fun hookRobotIcon()
+{
+    RobotIcon.setOnClickListener { view ->
+        Activity.startActivity( Intent( Activity, ConfigActivity::class.java ) );
+    }
+
+    Activity.runOnUiThread {
+        RobotIcon.setBackgroundResource( R.drawable.animated_robot_icon );
+
+        ( RobotIcon.background as AnimationDrawable ).start();
+    }
+}
+
+fun unhookRobotIcon()
+{
+    RobotIcon.setOnClickListener { view ->
+        // do nothing!
+    };
+
+    Activity.runOnUiThread {
+        RobotIcon.setBackgroundResource( R.drawable.robot_icon_off );
+    }
 }
