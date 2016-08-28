2453dc8cca408365a97ef96165e0e9b2c94a1516
addonovan <addonovan@users.noreply.github.com>
2016-08-27 21:37:04 -0500

    Added KPushBot samples
    This shows how the HardwarePushBot and PushBotTeleOpTank_Itarative
    classes would be written with kftc.

--- TeamCode/src/main/java/kftc/examples/KPushBot.kt
+++ TeamCode/src/main/java/kftc/examples/KPushBot.kt
@@ -23,9 +23,71 @@
  */
 package kftc.examples
 
+import addonovan.kftc.KOpMode
+import com.qualcomm.robotcore.util.Range
+
 /**
  * A port of the PushBot example to the kftc framework.
  *
  * @author addonovan
  * @since 8/27/16
  */
+@Suppress( "unused" )
+class KPushBot : KOpMode()
+{
+
+    /**
+     * This companion object allows the hardware to be completely
+     * initialized in another file, yet gives us full access to it
+     * so we can access it like all the methods and fields were
+     * initialized in this class. Neat, huh?
+     */
+    companion object : KPushBotHardware();
+
+    /** Servo mid position */
+    var clawOffset = 0.0;
+
+    /** sets rate to move servo */
+    // question: Why are the Qualcomm comments so terse and uninformative??
+    val CLAW_SPEED = 0.02;
+
+    //
+    // Overrides
+    //
+
+    override fun init()
+    {
+        Telemetry.addData( "Say", "Hello Driver" );
+        Telemetry.update();
+    }
+
+    override fun loop()
+    {
+        // Run wheels in tank mode (note: the joystick goes negative when pushed forwards, so negate it)
+        val left = -Gamepad1.left_stick_y.toDouble();
+        val right = -Gamepad1.right_stick_y.toDouble();
+        leftMotor.power = left;
+        rightMotor.power = right;
+
+        // Use gamepad left & right bumpers to open and close the claw
+        if ( Gamepad1.right_bumper ) clawOffset += CLAW_SPEED;
+        else if ( Gamepad1.left_bumper ) clawOffset -= CLAW_SPEED;
+
+        // move both servos to new position. Assume servos are mirror image of each other.
+        clawOffset = Range.clip( clawOffset, -0.5, 0.5 );
+        leftClaw.position = KPushBotHardware.MID_SERVO + clawOffset;
+        rightClaw.position = KPushBotHardware.MID_SERVO - clawOffset;
+
+        // use gamepad buttons to move the arm up (Y) and down (A)
+        if ( Gamepad1.y ) armMotor.power = KPushBotHardware.ARM_UP_POWER;
+        else if ( Gamepad1.a ) armMotor.power = KPushBotHardware.ARM_DOWN_POWER;
+        else armMotor.power = 0.0;
+
+        // send telemetry message to signify robot running;
+        Telemetry.addData( "claw", "Offset = %.2f", clawOffset );
+        Telemetry.addData( "left", "%.2f", left );
+        Telemetry.addData( "right", "%.2f", right );
+        Telemetry.update();
+    }
+
+}
